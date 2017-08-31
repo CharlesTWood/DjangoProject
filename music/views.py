de9d22aed315ef0ef4ba6f430ff606a35997a293
Charles <spudzy95@gmail.com>
2017-08-31 14:25:08 -0400

    First Commit

---
+++ music/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
+
+
+def index(request):
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums})
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        select_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+                       'album': album,
+                       'error_message': "No valid song ID was present"})
+    else:
+        select_song.is_favorite = True
+        select_song.save()
+        return render(request, 'music/detail.html', {'album': album})
+
+
